8f4e8167c8205a0bcc6a5ceb2374f9d6089cb51a
DavidChenMe <bhvs6p57g@hotmail.com>
2025-09-23 21:15:58 +0800

    add faucet

--- contracts/cUSDT.sol
+++ contracts/cUSDT.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
 import {ConfidentialFungibleToken} from "new-confidential-contracts/token/ConfidentialFungibleToken.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
-import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
 
 contract cUSDT is ConfidentialFungibleToken, SepoliaConfig {
     constructor() ConfidentialFungibleToken("cUSDT", "cUSDT", "") {}
+
+    // Faucet mint: anyone can mint confidential tokens to themselves.
+    // The amount is provided as an external encrypted input to preserve privacy.
+    function faucetMint(externalEuint64 encryptedAmount, bytes calldata inputProof) external returns (euint64) {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        // Mint to caller
+        euint64 minted = _mint(msg.sender, amount);
+        return minted;
+    }
 }
